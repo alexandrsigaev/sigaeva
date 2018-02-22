204e7fbf2b7b554ee200dd348acd27ffb02db8bd
Sigaev Aleksandr <sigaev.aleksandr.v@yandex.ru>
2018-02-22 20:43:48 +0300

    5. Рефакторинг теста @Before @After

--- chapter_002/src/test/java/ru/job4j/geometric/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/geometric/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.geometric;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -14,11 +16,22 @@ import static org.junit.Assert.assertThat;
  * @since 22.02.2018
  */
 public class PaintTest {
+
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(stdout);
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(new String(out.toByteArray()), is(
                 new StringBuilder()
@@ -37,9 +50,6 @@ public class PaintTest {
 
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         assertThat(new String(out.toByteArray()), is(
                 new StringBuilder()
