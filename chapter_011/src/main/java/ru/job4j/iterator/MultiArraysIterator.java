4fc749817c583ae012a5aaba08b6e2e8e9c91036
Sigaev Aleksandr <sigaev.aleksandr.v@yandex.ru>
2018-06-28 00:05:18 +0300

    Итератор для двухмерного массива int[][]

---
+++ chapter_011/src/main/java/ru/job4j/iterator/MultiArraysIterator.java
@@ -0,0 +1,46 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+
+/**
+ * Class
+ *
+ * @author sigaevaleksandr
+ * @since 27.06.2018
+ */
+public class MultiArraysIterator implements Iterator {
+
+    private final int[][] source;
+    private final int size;
+    private int countIndex = 0;
+    private int countLine = 0;
+    private int countColumn = 0;
+
+    public MultiArraysIterator(final int[][] source) {
+        this.source = source;
+        this.size = Stream.of(source).mapToInt(m -> m.length).sum();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return size > countIndex;
+    }
+
+    @Override
+    public Object next() {
+        int result;
+        if (countIndex == size){
+            throw new NoSuchElementException();
+        }else if (countColumn == source[countLine].length - 1){
+            result = source[countLine++][countColumn];
+            countColumn = 0;
+        } else {
+            result = source[countLine][countColumn++];
+        }
+        countIndex++;
+
+        return result;
+    }
+}
---
+++ chapter_011/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Class
+ *
+ * @author sigaevaleksandr
+ * @since 27.06.2018
+ */
+package ru.job4j;
