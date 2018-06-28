dea4afde021d7b90904f19f2032dcd7fdf6ac233
Sigaev Aleksandr <sigaev.aleksandr.v@yandex.ru>
2018-06-29 01:09:28 +0300

    Создать итератор простые числа.

---
+++ chapter_011/src/main/java/ru/job4j/iterator/PrimesIterator.java
@@ -0,0 +1,66 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Class
+ *
+ * @author sigaevaleksandr
+ * @since 28.06.2018
+ */
+public class PrimesIterator implements Iterator {
+
+    private final int[] source;
+    private int count = 0;
+
+    public PrimesIterator(int[] source) {
+        this.source = source;
+    }
+
+
+    @Override
+    public boolean hasNext() {
+        return findPrime()[0] != -1;
+    }
+
+    @Override
+    public Object next() {
+        int [] resilt = findPrime();
+        if (resilt[1] == -1){
+            throw new NoSuchElementException();
+        }
+        count = resilt[0] + 1;
+        return resilt[1];
+    }
+
+
+    private boolean isPrime(int num){
+        boolean result = true;
+        if (num < 2) {
+            result = false;
+        }else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    private int[] findPrime() {
+        int[] result = new int[] {-1, -1};
+
+        for (int i = count; i < source.length; i++) {
+            if (isPrime(source[i])) {
+                result[0] = i;
+                result[1] = source[i];
+                break;
+            }
+        }
+
+        return result;
+    }
+}
