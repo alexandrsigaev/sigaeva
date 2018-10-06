ba9c70df4fea7a82979e6a69dd9471ec229aac98
Sigaev Aleksandr <sigaev.aleksandr.v@yandex.ru>
2018-10-06 17:50:46 +0300

    Исправление Junit тест для блокирующей очереди. С учетом задания.

--- chapter_012/src/test/java/ru/job4j/waitnotify/SimpleBlockingQueueTest.java
+++ chapter_012/src/test/java/ru/job4j/waitnotify/SimpleBlockingQueueTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -23,29 +24,33 @@ public class SimpleBlockingQueueTest {
         ArrayList<Integer> buffer = new ArrayList<>();
 
         Thread producer = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
-                try {
-                    queue.offer(i);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+            IntStream.range(0, 10).forEach(
+                    value -> {
+                        try {
+                            queue.offer(value);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+            );
         });
 
         Thread consumer = new Thread(() -> {
-           while (!queue.isEmpty()) {
+           while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
                try {
                    buffer.add(queue.pool());
                } catch (InterruptedException e) {
                    e.printStackTrace();
+                   Thread.currentThread().interrupt();
                }
            }
         });
 
         producer.start();
         consumer.start();
-
         producer.join();
+        consumer.interrupt();
         consumer.join();
 
         assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)));
