a872dd96c175b5fd97d622f9bce2e528deb81cc5
Sigaev Aleksandr <sigaev.aleksandr.v@yandex.ru>
2019-01-24 14:12:57 +0300

    Создать web проект

---
+++ chapter_021/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -0,0 +1,24 @@
+package ru.job4j.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Class
+ *
+ * @author sigaevaleksandr
+ * @since 24.01.2019
+ */
+public class EchoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = new PrintWriter(resp.getOutputStream());
+        printWriter.append("hello world");
+        printWriter.flush();
+    }
+}
